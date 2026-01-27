5dc22e992c9d472f75ede3e900eaab10c827b69f
AilinMari <ailin_scilla@hotmail.com>
2026-01-27 11:38:10 +0100

    Search bar works with the filters available

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -125,7 +125,7 @@ export default function Home() {
                 value={searchQuery}
                 onChange={(e) => setSearchQuery(e.target.value)}
                 placeholder="Search for a venue.."
-                className="flex-1 px-4 py-3 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+                className="flex-1 px-4 py-3 border border-gray-300 rounded-s-md text-gray-900 placeholder-gray-500 focus:ring-blue-500 focus:border-blue-500"
               />
               
               <input
@@ -133,7 +133,7 @@ export default function Home() {
                 value={location}
                 onChange={(e) => setLocation(e.target.value)}
                 placeholder="Location"
-                className="w-full md:w-40 px-4 py-3 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+                className="w-full md:w-40 px-4 py-3 border border-gray-300 text-gray-900 placeholder-gray-500 focus:ring-blue-500 focus:border-blue-500"
               />
               
               <input
@@ -142,7 +142,7 @@ export default function Home() {
                 onChange={(e) => setGuests(e.target.value ? parseInt(e.target.value) : '')}
                 placeholder="Guests"
                 min="1"
-                className="w-full md:w-32 px-4 py-3 border border-gray-300  focus:ring-blue-500 focus:border-blue-500"
+                className="w-full md:w-32 px-4 py-3 border border-gray-300 text-gray-900 placeholder-gray-500 focus:ring-blue-500 focus:border-blue-500"
               />
               
               <input
@@ -151,12 +151,12 @@ export default function Home() {
                 onChange={(e) => setDateFrom(e.target.value)}
                 placeholder="Check in"
                 min={new Date().toISOString().split('T')[0]}
-                className="w-full md:w-40 px-4 py-3 border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+                className="w-full md:w-40 px-4 py-3 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500"
               />
               
               <button
                 type="submit"
-                className="bg-blue-600 text-white px-6 py-3 hover:bg-blue-700 font-medium flex items-center justify-center"
+                className=" text-gray-300 px-6 py-3 rounded-e-md hover:bg-blue-700 font-medium flex items-center justify-center"
               >
                 <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />

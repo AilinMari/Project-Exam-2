a5d25fdfdcbfb3ad317bf964809a406d09fdc743
AilinMari <ailin_scilla@hotmail.com>
2026-01-27 13:32:38 +0100

    links in the carousel fixed. The links were off.

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="venue/:id" element={<VenueDetails />} />
+          <Route path="venues/:id" element={<VenueDetails />} />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
           <Route path="profile" element={<Profile />} />
